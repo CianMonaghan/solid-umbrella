129315af5140aab177d443774b4017c7aa1edc10
Cian Monaghan <ciancmonaghan@gmail.com>
2025-02-19 06:22:04 +0000

    Did the homework - it is not good

--- ls2.c
+++ ls2.c
@@ -7,12 +7,31 @@
 #include "ls2.h"
 
 
-// TODO: function definitions here for ls2
-
 /**
- * This is just an example. Delete this before 
- * submission.
+ * Builds the path from the inputed path name
+ * and puts it into a path string
  */
-void example(int* x) {
-    *x = thisIsGlobal;
+void buildPath(char* pathName[], char* path[]) {
+    struct stat*restrict pathStat;
+    stat(pathName, pathStat);
+    while(S_ISREG(pathStat->st_mode)){//isn't normal file
+        struct dirent pathDirent;
+        DIR* pathDIR = opendir(pathName);
+        readdir(pathDIR);
+        if(S_ISDIR(pathStat->st_mode)){//is dir
+            strcat(path, INDENT);
+            strcat(path, pathDirent.d_name);
+            buildPath(pathDirent.d_name, path); //recursion
+        }
+        //i know this is bad form
+        int leafSize = pathStat->st_size;
+        strcat(path, INDENT);
+        strcat(path, pathDirent.d_name);
+        strcat(path, " (Size: ");
+        strcat(path, leafSize);
+        strcat(path, " bytes)");
+        closedir(pathDIR);
+        //this probably doesn't work
+    }
+
 } 
--- main.c
+++ main.c
@@ -8,17 +8,32 @@
  * Usage: ls2 <path> [exact-match-pattern]
  */
 int main(int argc, char* argv[]) {
-	// stack stores the lines to print out
-	stack_t *s = initstack();
+	if(argc == 2){//mode 1
+
+		//get the file name and run the build on it
+		char* pathName[] = argv[1];
+		char* path[] = (char)malloc(10000*sizeof(char)); //i know this is bad form, i have no other idea what to do
+		buildPath(pathName, path);
+		printf("%s\n", path);
+
+	} else if(argc == 3){//mode 2
+		//David said I don't have to do this section
+
+		//stack stores the lines to print out
+	/* stack_t *s = initstack();
 
 	push(s, "Hello1");
 	push(s, "Hello2");
 	push(s, "Hello3");
 
-	// print stack
+	// print stackn 
 	printstack(s);
 
 	// free up stack
-	freestack(s);
+	freestack(s); */
+	} else {
+		printf("Usage: ls2 <path> [exact-match-pattern]");
+	}
+	
 	return 0;
 }
